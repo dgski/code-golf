458954cd378284929463ffcce1785f1307ff79c6
dgski <gorski.dave@gmail.com>
2018-10-13 12:24:18 -0400

    Added Yeah But No

---
+++ yeahbutno.py
@@ -0,0 +1,33 @@
+'''
+Input: 0
+Output:
+
+
+Input: 1
+Output:
+Yeah
+But
+
+Input: 2
+Output:
+Yeah
+But
+No
+
+Input: 10
+Output:
+Yeah
+But
+No
+But
+Yeah
+But
+No
+Yeah
+But
+No
+But
+Yeah
+'''
+
+f=lambda n:f(n-1)+'Yeah\n'*((n-1)%3<1)+'But\n'*((n-1)%2<1)+'No\n'*((n-1)%3==1)if n else""
